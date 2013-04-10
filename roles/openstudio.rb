bae7c4c06f06caf03c9308dbfb10bf0c37c2668b
Nicholas Long <nicholas.long@nrel.gov>
2013-04-10 14:04:25 -0600

    fixing default_attributes

--- roles/openstudio.rb
+++ roles/openstudio.rb
@@ -20,15 +20,17 @@ default_attributes(
                 { "name" => "bundler"},
                 { "name" => "rake"},
             ]
-        },
-        :openstudio => {
-            :version => "0.10.5.11322",
-            :checksum => "9180659c77a7fc710cb9826d40ae67c65db0d26bb4bce1a93b64d7e63f4a1f2c"
-        },
-        :energyplus => {
-            :version => "7.2.0.006",
-            :checksum => "c1ec1499f964bad8638d3c732c9bd10793dd4052a188cd06bb49288d3d962e09"
         }
-    })
+    },
+
+    :openstudio => {
+        :version => "0.10.5.11322",
+        :checksum => "9180659c77a7fc710cb9826d40ae67c65db0d26bb4bce1a93b64d7e63f4a1f2c"
+    },
+    :energyplus => {
+        :version => "7.2.0.006",
+        :checksum => "c1ec1499f964bad8638d3c732c9bd10793dd4052a188cd06bb49288d3d962e09"
+    }
+)
 
 
